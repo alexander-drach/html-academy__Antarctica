22a6a76a1e2382c817310672e416b6f27c254844
Alexander Drach <alexander.drach.adci@gmail.com>
2023-10-21 14:01:58 +0600

    mobile menu

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,6 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {Form} from './modules/form-validate/form';
+import {Burger} from './modules/burger/burger';
 
 // ---------------------------------
 
@@ -19,6 +20,9 @@ window.addEventListener('DOMContentLoaded', () => {
     const form = new Form();
     window.form = form;
     form.init();
+
+    const burger = new Burger();
+    burger.init();
   });
 });
 
